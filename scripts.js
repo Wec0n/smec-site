b56248396a9e711befafa37c58c9582581dfbc14
Willian Silva <willsoar9620@gmail.com>
2022-08-15 21:53:20 -0300

    Realizadas várias melhorias nos códigos

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
-const sub = document.querySelectorAll('.lista-item');
-
-sub.forEach((item, index) => {
-    item.querySelector('.item-link').addEventListener('click', () => {
-        if ((item.querySelector('.item-submenu').style.maxHeight != '250px')) {
-            item.querySelector('.item-submenu').style.maxHeight = '250px';
-        } else {
-            item.querySelector('.item-submenu').style.maxHeight = '0px';
-        }
-
-        sub.forEach((outro) => {
-            if (outro.querySelector('.item-submenu') != null || outro.querySelector('.item-submenu') != undefined) {
-                if (item != outro) {
-                    outro.querySelector('.item-submenu').style.maxHeight = '0px';
+window.onload = function () {
+    const sub = document.querySelectorAll('.lista-item');
+    sub.forEach((item) => {
+        item.querySelector('.item-link').addEventListener('click', () => {
+            if (item.querySelector('.item-submenu') != (null || undefined)) {
+                if ((item.querySelector('.item-submenu').style.maxHeight != '250px')) {
+                    item.querySelector('.item-submenu').style.maxHeight = '250px';
+                } else {
+                    item.querySelector('.item-submenu').style.maxHeight = '0px';
                 }
             }
+
+            sub.forEach((outro) => {
+                if (outro.querySelector('.item-submenu') != (null || undefined)) {
+                    if (item != outro) {
+                        outro.querySelector('.item-submenu').style.maxHeight = '0px';
+                    }
+                }
+            })
         })
     })
-})
+}
