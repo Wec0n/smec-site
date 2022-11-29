1995398a3653038fb32dd54474702f0d833d4af6
Willian Silva <willsoar9620@gmail.com>
2022-11-29 13:15:55 -0300

    Modificado o menu do dashboard. Agora o submenu do link clicado fica aberto

--- Dashboard/dashboard-scripts.js
+++ Dashboard/dashboard-scripts.js
@@ -2,7 +2,7 @@ window.onload = () => { // O CÓDIGO SÓ VAI EXECUTAR QUANDO TODA A PÁGINA FOR 
     (() => {
         const sub = document.querySelectorAll('.lista-item'); // SELECIONA TODOS OS "LI" QUE CONTENHAM A CLASSE ".lista-item".
         sub.forEach((item) => { // PERCORRE TODOS OS "LI" QUE CONTENHAM A CLASSE ".lista-item".
-            item.addEventListener('click', () => { // AO CLICAR EM UM "LI", EXECUTA OS SEGUINTES CÓDIGOS:
+            item.addEventListener('click', (event) => { // AO CLICAR EM UM "LI", EXECUTA OS SEGUINTES CÓDIGOS:
                 // SE O ITEM CLICADO CONTÉM OPACIDADE, ESTA É REMOVIDA, POIS O ITEM CLICADO É O FOCO .
                 if (item.classList.contains('menu-opacidade')) {
                     item.classList.remove('menu-opacidade');
@@ -13,9 +13,20 @@ window.onload = () => { // O CÓDIGO SÓ VAI EXECUTAR QUANDO TODA A PÁGINA FOR 
                         item.querySelector('.item-submenu').style.maxHeight = '250px';
                     } else {
                         item.querySelector('.item-submenu').style.maxHeight = '0px';
+                        // VERIFICAÇÃO PARA FICAR ABERTO O SUBMENU DO ITEM SELECIONADO
+                        // FALTA AINDA COLOCAR O ESTILO DE HOVER QUANDO SELECIONADO
+                        item.querySelectorAll('.item-submenu').forEach((submenuDoItem) => {
+                            submenuDoItem.querySelectorAll('.submenu-subitem').forEach((subitemDoSubmenu) => {
+                                let linkDoSubmenu = subitemDoSubmenu.querySelector('.subitem-link');
+                                if (linkDoSubmenu == event.target) {
+                                    item.querySelector('.item-submenu').style.maxHeight = '250px';
+                                }
+                            })
+                        })
                     }
                 }
-    
+                // console.log(item.querySelector('.item-submenu'));
+
                 sub.forEach((outro) => { // SELECIONA OS ITENS QUE NÃO O QUE FOI CLICADO
                     // ADICIONA OPACIDADE AOS ITENS DO MENU QUE NÃO SÃO O FOCO
                     if (item != outro) {
@@ -53,38 +64,4 @@ window.onload = () => { // O CÓDIGO SÓ VAI EXECUTAR QUANDO TODA A PÁGINA FOR 
             })
         })
     })();
-
-    // (() => {
-    //     let btn = null;
-    //     document.querySelectorAll('a').forEach((itemDoMenu) => {
-    //         if (itemDoMenu.innerHTML == 'Cadastro') {
-    //             btn = itemDoMenu;
-    //         }
-    //     });
-    //     btn.addEventListener('click', () => {
-    //         const caminhoTelaTrabalho = fetch('tela-trabalho-usuario-cadastro.html');
-    //         caminhoTelaTrabalho.then(r => r.text()).then(body => {
-    //             const conteudo = document.querySelector('.tela-trabalho');
-    //             conteudo.innerHTML = body;
-    //         });
-    //     });
-    // })();
-
-    // (() => {
-    //     let btn = null;
-    //     document.querySelectorAll('a').forEach((itemDoMenu) => {
-    //         if (itemDoMenu.innerHTML == 'Listar') {
-    //             btn = itemDoMenu;
-    //         }
-    //     });
-    //     btn.addEventListener('click', () => {
-    //         const caminhoTelaTrabalho = fetch('tela-trabalho-usuario-cadastro.html');
-    //         caminhoTelaTrabalho.then(r => r.text()).then(body => {
-    //             const conteudo = document.querySelector('.tela-trabalho');
-    //             conteudo.innerHTML = body;
-    //         });
-    //     });
-    // })();
-
-    //REALIZAÇÃO DE TESTES
 }
